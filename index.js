9a03e2834113e0e0632126ee9421ea1b8edc2ed3
Simon Jentsch <simon.jentsch@9cookies.com>
2016-12-02 16:47:09 +0100

    add request models

--- index.js
+++ index.js
@@ -11,65 +11,81 @@ class ServerlessModelPlugin {
     };
   }
 
-  beforeDeployFunctions() {
+  addModels(resources, models, modelName) {
+    const model = {
+      Type: 'AWS::ApiGateway::Model',
+      Properties: {
+        RestApiId: {
+          Ref: 'ApiGatewayRestApi',
+        },
+        ContentType: models[modelName].ContentType,
+        Name: modelName,
+        Schema: models[modelName].Schema,
+      },
+    };
+    resources[`${modelName}Model`] = model;
+  }
 
+  addModelDependencies(models, resource) {
+    Object.keys(models).forEach(contentType => {
+      resource.DependsOn.add(`${models[contentType]}Model`);
+    });
+  }
+
+  addMethodResponses(resource, httpEvt) {
+    if (httpEvt.methodResponses) {
+      resource.Properties.MethodResponses = [];
+
+      httpEvt.methodResponses.forEach(response => {
+        this.addModelDependencies(response.ResponseModels, resource);
+        resource.Properties.MethodResponses.push(response);
+      });
+    }
+  }
+
+  addRequestModels(resource, httpEvt) {
+    if (httpEvt.requestModels) {
+      this.addModelDependencies(httpEvt.requestModels, resource);
+      resource.Properties.RequestModels = httpEvt.requestModels;
+    }
+  }
+
+  beforeDeployFunctions() {
     const naming = this.serverless.providers.aws.naming;
     const getMethodLogicalId = naming.getMethodLogicalId.bind(naming);
     const normalizePath = naming.normalizePath.bind(naming);
+    const customVars = this.serverless.variables.service.custom;
 
-    let models;
-    if (this.serverless.variables.service.custom) {
-      models = this.serverless.variables.service.custom.models;
+    if (!(customVars && customVars.models)) {
+      return;
     }
 
     const provider = this.serverless.service.provider;
     const cfTemplate = provider.compiledCloudFormationTemplate;
 
     // Add model resources
-    for (const modelName in models) {
-      if (models.hasOwnProperty(modelName)) {
-        const model = {
-          Type: 'AWS::ApiGateway::Model',
-          Properties: {
-            RestApiId: {
-              Ref: 'ApiGatewayRestApi',
-            },
-            ContentType: models[modelName].ContentType,
-            Name: modelName,
-            Schema: models[modelName].Schema,
-          },
-        };
-        cfTemplate.Resources[modelName + 'Model'] = model;
-      }
-    }
+    Object.keys(customVars.models).forEach(modelName => {
+      this.addModels(cfTemplate.Resources, customVars.models, modelName);
+    });
 
+    // Add models to method resources
     this.serverless.service.getAllFunctions().forEach(functionName => {
       const func = this.serverless.service.getFunction(functionName)
       func.events.forEach(eventTypes => {
-        if (eventTypes.http && eventTypes.http.methodResponses) {
-          const methodResponses = eventTypes.http.methodResponses;
+        if (eventTypes.http) {
           const resourceName = normalizePath(eventTypes.http.path);
           const methodLogicalId = getMethodLogicalId(resourceName, eventTypes.http.method);
           const resource = cfTemplate.Resources[methodLogicalId];
-          resource.Properties.MethodResponses = [];
-          methodResponses.forEach(response => {
-            const ResponseModels = response.Models;
-            const StatusCode = response.StatusCode;
-            resource.Properties.MethodResponses.push({
-              ResponseModels,
-              StatusCode,
-            });
-          });
+          resource.DependsOn = new Set();
+          this.addMethodResponses(resource, eventTypes.http);
+          this.addRequestModels(resource, eventTypes.http);
+          resource.DependsOn = Array.from(resource.DependsOn);
+          if (resource.DependsOn.length === 0) {
+            delete resource.DependsOn;
+          }
         }
       });
     });
-
-
-    // console.log('############');
-    // console.log(this.serverless.providers.aws);
-
-    // console.log('res', provider.compiledCloudFormationTemplate.Resources);
-
   }
 
 }
