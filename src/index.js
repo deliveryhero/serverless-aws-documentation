917e2c307166358f3d648c3a7ede91ded9632a10
Simon Jentsch <simon.jentsch@9cookies.com>
2016-12-09 12:06:34 +0100

    add global and function documentation parts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 'use strict';
+const AWS = require('aws-sdk');
 
 class ServerlessAwsModels {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
     this.provider = 'aws'
+
+    this.customVars = this.serverless.variables.service.custom;
+    const naming = this.serverless.providers.aws.naming;
+    this.getMethodLogicalId = naming.getMethodLogicalId.bind(naming);
+    this.normalizePath = naming.normalizePath.bind(naming);
 
     this._beforeDeploy = this.beforeDeploy.bind(this)
     this._afterDeploy = this.afterDeploy.bind(this)
@@ -17,19 +23,29 @@ class ServerlessAwsModels {
     this.documentationParts = [];
   }
 
-  addModels(resources, models, modelName) {
-    const model = {
+  static transformToCfKeys(resource) {
+    const _resource = Object.assign({}, resource);
+    Object.keys(_resource).forEach((key) => {
+      const cfKey = key.charAt(0).toUpperCase() + key.slice(1);
+      _resource[cfKey] = _resource[key];
+      delete _resource[key];
+    });
+
+    return _resource;
+  }
+
+  createCfModel(model) {
+    return {
       Type: 'AWS::ApiGateway::Model',
       Properties: {
         RestApiId: {
           Ref: 'ApiGatewayRestApi',
         },
-        ContentType: models[modelName].ContentType,
-        Name: modelName,
-        Schema: models[modelName].Schema,
+        ContentType: model.contentType,
+        Name: model.name,
+        Schema: model.schema,
       },
     };
-    resources[`${modelName}Model`] = model;
   }
 
   addModelDependencies(models, resource) {
@@ -43,8 +59,13 @@ class ServerlessAwsModels {
       resource.Properties.MethodResponses = [];
 
       httpEvt.methodResponses.forEach(response => {
-        this.addModelDependencies(response.ResponseModels, resource);
-        resource.Properties.MethodResponses.push(response);
+        const _response = {
+          StatusCode: response.statusCode,
+          ResponseModels: response.responseModels,
+        };
+
+        this.addModelDependencies(_response.ResponseModels, resource);
+        resource.Properties.MethodResponses.push(_response);
       });
     }
   }
@@ -56,41 +77,39 @@ class ServerlessAwsModels {
     }
   }
 
-  beforeDeploy() {
-    const naming = this.serverless.providers.aws.naming;
-    const getMethodLogicalId = naming.getMethodLogicalId.bind(naming);
-    const normalizePath = naming.normalizePath.bind(naming);
-    const customVars = this.serverless.variables.service.custom;
+  updateCfTemplateFromHttp(eventTypes) {
+    if (eventTypes.http && eventTypes.http.documentation) {
+      const resourceName = this.normalizePath(eventTypes.http.path);
+      const methodLogicalId = this.getMethodLogicalId(resourceName, eventTypes.http.method);
+      const resource = this.cfTemplate.Resources[methodLogicalId];
 
-    if (!(customVars && customVars.models)) {
-      return;
+      resource.DependsOn = new Set();
+      this.addMethodResponses(resource, eventTypes.http.documentation);
+      this.addRequestModels(resource, eventTypes.http.documentation);
+      resource.DependsOn = Array.from(resource.DependsOn);
+      if (resource.DependsOn.length === 0) {
+        delete resource.DependsOn;
+      }
     }
+  }
 
-    const provider = this.serverless.service.provider;
-    const cfTemplate = provider.compiledCloudFormationTemplate;
+  beforeDeploy() {
+    if (!(this.customVars && this.customVars.documentation.models)) return;
+
+    this.cfTemplate = this.serverless.service.provider.compiledCloudFormationTemplate;
 
     // Add model resources
-    Object.keys(customVars.models).forEach(modelName => {
-      this.addModels(cfTemplate.Resources, customVars.models, modelName);
-    });
+    const models = this.customVars.documentation.models.map(this.createCfModel)
+      .reduce((modelObj, model) => {
+        modelObj[`${model.Properties.Name}Model`] = model;
+        return modelObj;
+      }, {});
+    Object.assign(this.cfTemplate.Resources, models);
 
     // Add models to method resources
     this.serverless.service.getAllFunctions().forEach(functionName => {
       const func = this.serverless.service.getFunction(functionName);
-      func.events.forEach(eventTypes => {
-        if (eventTypes.http && eventTypes.http.documentation) {
-          const resourceName = normalizePath(eventTypes.http.path);
-          const methodLogicalId = getMethodLogicalId(resourceName, eventTypes.http.method);
-          const resource = cfTemplate.Resources[methodLogicalId];
-          resource.DependsOn = new Set();
-          this.addMethodResponses(resource, eventTypes.http.documentation);
-          this.addRequestModels(resource, eventTypes.http.documentation);
-          resource.DependsOn = Array.from(resource.DependsOn);
-          if (resource.DependsOn.length === 0) {
-            delete resource.DependsOn;
-          }
-        }
-      });
+      func.events.forEach(this.updateCfTemplateFromHttp.bind(this));
     });
   }
 
@@ -106,7 +125,7 @@ class ServerlessAwsModels {
 
   static _mapToObj(map) {
     const returnObj = {};
-    map.forEach((key, val) => {
+    map.forEach((val, key) => {
       returnObj[key] = val;
     });
 
@@ -115,7 +134,7 @@ class ServerlessAwsModels {
 
   _createDocumentationPart(part, def, knownLocation) {
     const location = part.locationProps.reduce((loc, property) => {
-      loc[property] = knownLocation[property] || part.property;
+      loc[property] = knownLocation[property] || def[property];
       return loc;
     }, {});
     location.type = part.type;
@@ -153,74 +172,74 @@ class ServerlessAwsModels {
     });
   }
 
+  _updateDocumentation() {
+    const apiGateway = new AWS.APIGateway(this.serverless.providers.aws.getCredentials());
+    return apiGateway.getDocumentationParts({
+      restApiId: this.restApiId,
+      limit: 9999,
+    }).promise()
+      .then(results => results.items.map(part => apiGateway.deleteDocumentationPart({
+        documentationPartId: part.id,
+        restApiId: this.restApiId,
+      }).promise()))
+      .then(promises => Promise.all(promises))
+      .then(() => this.documentationParts.map(part => {
+        part.properties = JSON.stringify(part.properties);
+        return apiGateway.createDocumentationPart(part).promise();
+      }))
+      .then(promises => Promise.all(promises));
+  }
 
-  afterDeploy() {
-    const customVars = this.serverless.variables.service.custom;
-    const generalDocumentation = customVars.documentation || {};
+  getGlobalDocumentationParts() {
+    const globalDocumentation = this.customVars.documentation || {};
+    this.createDocumentationParts(ServerlessAwsModels._globalDocumentationParts, globalDocumentation, {});
+  }
 
+  getFunctionDocumentationParts() {
     this.serverless.service.getAllFunctions().forEach(functionName => {
       const func = this.serverless.service.getFunction(functionName);
       func.events.forEach(eventTypes => {
         if (eventTypes.http && eventTypes.http.documentation) {
           const path = eventTypes.http.path;
-          const method = eventTypes.http.method;
+          const method = eventTypes.http.method.toUpperCase();
           this.createDocumentationParts(ServerlessAwsModels._functionDocumentationParts, eventTypes.http, { path, method });
         }
       });
     });
+  }
 
-    console.log('docParts', this.documentationParts);
+  _buildDocumentation(result) {
+    this.restApiId = result.Stacks[0].Outputs
+      .filter(output => output.OutputKey === 'ApiId')
+      .map(output => output.OutputValue)[0];
+
+    this.getGlobalDocumentationParts();
+    this.getFunctionDocumentationParts();
+
+    if (this.options.noDeploy) {
+      console.info('-------------------');
+      console.info('documentation parts:');
+      console.info(this.documentationParts);
+      return;
+    }
+
+    this._updateDocumentation();
+  }
+
+  afterDeploy() {
+    if (!this.customVars.documentation || !this.customVars.documentation.version) return;
+    const stackName = this.serverless.providers.aws.naming.getStackName(this.options.stage);
+    this.serverless.providers.aws.request('CloudFormation', 'describeStacks', { StackName: stackName },
+      this.options.stage,
+      this.options.region
+    ).then(this._buildDocumentation.bind(this));
   }
 
 }
 
 ServerlessAwsModels._documentationProperties = ['description'];
 
-ServerlessAwsModels._functionDocumentationParts = {
-  documentation: {
-    type: 'METHOD',
-    isList: false,
-    locationProps: ['path', 'method'],
-    children: {
-      requestBody: {
-        type: 'REQUEST_BODY',
-        isList: false,
-        locationProps: ['path', 'method'],
-      },
-      requestHeaders: {
-        type: 'REQUEST_HEADER',
-        isList: true,
-        locationProps: ['path', 'method', 'name'],
-      },
-      queryParams: {
-        type: 'QUERY_PARAMETER',
-        isList: true,
-        locationProps: ['path', 'method', 'name'],
-      },
-      pathParams: {
-        type: 'PATH_PARAMETER',
-        isList: true,
-        locationProps: ['path', 'method', 'name'],
-      },
-      methodResponses: {
-        type: 'PATH_PARAMETER',
-        isList: true,
-        locationProps: ['path', 'method', 'StatusCode'],
-        children: {
-          ResponseHeaders: {
-            type: 'RESPONSE_HEADER',
-            isList: true,
-            locationProps: ['path', 'method', 'name', 'StatusCode'],
-          },
-          ResponseBody: {
-            type: 'RESPONSE_BODY',
-            isList: false,
-            locationProps: ['path', 'method', 'StatusCode'],
-          }
-        }
-      }
-    }
-  }
-};
+ServerlessAwsModels._globalDocumentationParts = require('./globalDocumentationParts.json');
+ServerlessAwsModels._functionDocumentationParts = require('./functionDocumentationParts.json');
 
 module.exports = ServerlessAwsModels;
