949340f1da14f049647e162f4540e9b02e4f0fd3
Michael MacDonald <github@macdonald.cx>
2017-10-12 04:11:46 -0400

    Support "$ref": "{{model:MyModel}}" (#58)
    Dynamically inserts references to the model.
    Closes #21

---
+++ src/models.spec.js
@@ -0,0 +1,91 @@
+describe('ServerlessAWSDocumentation', function() {
+    const objectUnderTest = require('./models.js')
+
+    describe('createCfModel', () => {
+        it('should replace model ref with valid URI', () => {
+            let modelInput = {
+                contentType: 'application/json',
+                name: 'TestModel',
+                schema: {
+                    type: 'object',
+                    properties: {
+                        prop: {
+                            '$ref': '{{model: OtherModelName}}'
+                        }
+                    }
+                }
+            };
+
+            let modelOutput = objectUnderTest.createCfModel(modelInput);
+            expect(modelOutput).toEqual({
+                Type: 'AWS::ApiGateway::Model',
+                Properties: {
+                    RestApiId: {
+                        Ref: 'ApiGatewayRestApi',
+                    },
+                    ContentType: 'application/json',
+                    Name: 'TestModel',
+                    Schema: {
+                        type: 'object',
+                        properties: {
+                            prop: {
+                                '$ref': {
+                                    'Fn::Join': [
+                                        '/',
+                                        [
+                                            'https://apigateway.amazonaws.com/restapis',
+                                            {
+                                                'Ref': 'ApiGatewayRestApi'
+                                            },
+                                            'models',
+                                            'OtherModelName'
+                                        ]
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                },
+                DependsOn: [
+                    'OtherModelNameModel'
+                ]
+            });
+        });
+
+        it('should not mess with non-ref model definitions', () => {
+            let modelInput = {
+                contentType: 'application/json',
+                name: 'TestModel',
+                schema: {
+                    type: 'object',
+                    properties: {
+                        prop: {
+                            type: 'string'
+                        }
+                    }
+                }
+            };
+
+            let modelOutput = objectUnderTest.createCfModel(modelInput);
+            expect(modelOutput).toEqual({
+                Type: 'AWS::ApiGateway::Model',
+                Properties: {
+                    RestApiId: {
+                        Ref: 'ApiGatewayRestApi',
+                    },
+                    ContentType: 'application/json',
+                    Name: 'TestModel',
+                    Schema: {
+                        type: 'object',
+                        properties: {
+                            prop: {
+                                type: 'string'
+                            }
+                        }
+                    }
+                }
+            });
+        });
+
+    });
+})
